6bb286cdfdf37714cd9e0341d2f6e4a98b28215e
krdyr <106346651+krdyr@users.noreply.github.com>
2024-12-01 22:11:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
 from project_name_fetch import get_compose_project_name
 from container_checker import restart_inactive_containers
+
+# Override stdout to always flush
+sys.stdout = open(sys.stdout.fileno(), mode='w', buffering=1)
+
 def main():
   project_name = get_compose_project_name()
   restart_inactive_containers(project_name)
